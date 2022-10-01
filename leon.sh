f4d9a91e9bb649d25520791aadb83c215de23df4
YouKnowWhoRev <114790930+YouKnowWhoRev@users.noreply.github.com>
2022-10-01 13:22:28 +0800

    Add files via upload

---
+++ leon.sh
@@ -0,0 +1,30 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay KONTOL
+echo $purple"KONTOL LEON KECIL"
+echo
+sleep 2
+echo $yellow
+figlet "CHINA"
+echo $green"Yahaha China"
+echo $purple"CHINA GOBLOK"
+echo $red
+figlet "VINCENT"
+echo $blue
+figlet "VINCENT"
+echo $yellow
+figlet "VINCENT"
+echo $green
+figlet "VINCENT"
+echo $purple
+figlet "VINCENT"
+echo $white
+figlet "VINCENT"
